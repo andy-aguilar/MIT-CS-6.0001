e38179cd8eb7479ab5553868ea89fd5bacfe9fb3
andy-aguilar <aaguil3@gmail.com>
2021-02-01 14:15:03 -0600

    reorganized some folders

--- fin_exs/ch_3/string_dig.py
+++ fin_exs/ch_3/string_dig.py
@@ -1,12 +1,10 @@
+#adds up the digits in string "s" without using any methods that
+#haven't been covered in the book
+
 s = "1.23,2.4,13.123"
 total = 0
 current = 0
 dec = False
-
-# I know that the easy way to do this is to split at the commas
-# then convert to floats and then add but I wanted to use only 
-# what we've learned so far we haven't even learned functions 
-# yet so I couldn't even create a split function
 
 for d in s:
     if d == ",":
