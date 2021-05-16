b5b194a781d3d5b020020e98913a41e9d4bfe5cb
andy-aguilar <andres.aguilar@flatironschool.com>
2021-05-15 22:11:25 -0400

    Finish lecture 9

--- notes/lec9_inheritance.py
+++ notes/lec9_inheritance.py
@@ -44,7 +44,7 @@ c.set_name("fluffy")
 print(c)
 c.speak()
 print(c.get_age())
-#a.speak() # error because there is no speak method for Animal class
+# a.speak() # error because there is no speak method for Animal class
 
     
 #################################
@@ -78,6 +78,7 @@ print(p2.get_age())
 print(p1)
 p1.speak()
 p1.age_diff(p2)
+Person.age_diff(p1, p2)
 
 
 #################################
